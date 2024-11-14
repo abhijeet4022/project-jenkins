6996485fb2efa3dfc84130d418a100868691800e
abhijeet4022 <abhijeet4022@gmail.com>
2024-11-14 05:48:41 +0530

    Multi Branch Pipeline Jobs

---
+++ shared-library/vars/demo.groovy
@@ -0,0 +1,46 @@
+def call() {
+    // Declarative Pipeline - V2 Code.
+    pipeline {
+        agent { node { label 'workstation' } }
+
+        options {
+            ansiColor('xterm')
+            buildDiscarder(logRotator(numToKeepStr: '3'))
+        }
+
+        stages {
+            stage('CompileCode') {
+                steps {
+                    script {
+                        // Add valid shell commands here
+                        sh 'echo "Running compile stage"'
+                        sh 'pwd'
+                        sh 'ls -l'
+                    }
+                }
+            }
+
+            stage('Build') {
+                steps {
+                    echo "Hello World"
+                }
+            }
+
+            stage('Test') {
+                steps {
+                    echo "Test for pollSCM"
+                }
+            }
+        }
+
+        post {
+            always {
+                script {
+                    echo "Build finished"
+                    cleanWs()
+                }
+            }
+        }
+    }
+
+}

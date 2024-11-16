1384152335808b64a1cedfcf1fe412ccf07d0116
abhijeet4022 <abhijeet4022@gmail.com>
2024-11-16 15:45:12 +0530

    Code Compile

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -1,9 +1,26 @@
 // Compile Source Code and Build Artifacts.
 def CodeCompileAndBuilt() {
+
+
+//  For Payment and Frontend no need of code compilation.
+    if (env.codeType == "python" || env.codeType == "static") {
+        return "Return, Do not need Compilation"
+    }
+
     stage('Code Compile And Built Phase') {
-        echo 'CodeCompileAndBuilt'
+//      For Shipping Component.
+        if (env.codeType == "maven") {
+            sh '/home/centos/mavan/bin/mvn clean package'
+        }
+
+//      For Catalogue, User and Cart Component
+        if (env.codeType == "nodejs") {
+            sh 'npm install'
+        }
+
     }
 }
+
 
 // Executing Unit Testing Phase to Validate Code Functions.
 def UnitTest() {
